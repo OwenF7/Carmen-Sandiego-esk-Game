ca63d6d6b0c3bced13fd003c13564d173655c34d
OwenF7 <of752816@ohio.edu>
2021-03-22 23:55:35 -0400

    Update #3.5
    Trying to Commit again. Lots of changes and additions. Trying to split Update #3 Commit in thirds

--- Assets/Scripts/RaycastingCrosshair.cs
+++ Assets/Scripts/RaycastingCrosshair.cs
@@ -15,7 +15,14 @@ public class RaycastingCrosshair : MonoBehaviour
     public GameObject informationBox;
     public TextMeshProUGUI intel;
 
+    public GameObject reportBox;
+    public TextMeshProUGUI report;
+
+    public GameObject levelLoader; 
+
     public float raycastMaxDistance = 5.0f;
+
+    public bool reportReveal;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +34,8 @@ public class RaycastingCrosshair : MonoBehaviour
     void Update()
     {
         RaycastHit hit; 
+
+        //Raycast, if statements regarding date and interactions
 
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, raycastMaxDistance))
         {
@@ -40,7 +49,10 @@ public class RaycastingCrosshair : MonoBehaviour
             {
                 interactText.text = "Press E to send Agents to Taiwan";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Germany")
@@ -55,227 +67,347 @@ public class RaycastingCrosshair : MonoBehaviour
 
                 else if (hit.transform.tag == "Dec 1939")
                 {
-                    intel.text = "This would be the second text.";
+                    intel.text = " Country: Germany \n City: Berlin \n Occupation: \n Likelyhood to get caught: High \n Status: \n \n The belly of the beast. I shouldn't send agents here. It's too risky";
                 }
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Poland")
             {
                 interactText.text = "Press E to send Agents to Poland";
 
-                //if
+                if (hit.transform.tag == "Sept 1939")
+                {
+                    intel.text = " Country: Poland \n City: Warsaw \n Occupation: Poland \n Likelyhood to get caught: Medium \n Status: \n \n We recently got a request for a meeting with some disturbing " +
+                        "accusations. The Prime Minister seemed very on edge in the brief interaction with one of our agents. Something is coming.";
+                }
+
+                else if (hit.transform.tag == "Dec 1939")
+                {
+                    intel.text = " Country: Poland \n City: Warsaw \n Occupation: Germany \n Likelyhood to get caught: High \n Status: \n \n Germany invaded here in September. We may be able to gleam " +
+                        "some information out here...";
+                }
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "France")
             {
                 interactText.text = "Press E to send Agents to France";
 
-                //if
+                if (hit.transform.tag == "Sept 1939")
+                {
+                    intel.text = " Country: France \n City: Paris \n Occupation: France \n Likelyhood to get caught: Low \n Status: \n \n A meeting request with the French Spies. They are saying it is " +
+                        "urgent. We probably shouldn't keep them waiting.";
+                }
+
+                else if (hit.transform.tag == "Dec 1939")
+                {
+                    intel.text = " Country: France \n City: Paris \n Occupation: France \n Likelyhood to get caught: High \n Status: \n \n As we have feared, it seems that we are in the midst of another " +
+                        "World War...";
+                }
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Italy")
             {
                 interactText.text = "Press E to send Agents to Italy";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Spain")
             {
                 interactText.text = "Press E to send Agents to Germany";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Soviet Union")
             {
                 interactText.text = "Press E to send Agents to the Soviet Union";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Sweden")
             {
                 interactText.text = "Press E to send Agents to Sweden";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Finland")
             {
                 interactText.text = "Press E to send Agents to Finland";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Great Britain")
             {
                 interactText.text = "Press E to send Agents to Great Britain";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Algeria")
             {
                 interactText.text = "Press E to send Agents to Algeria";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Libya")
             {
                 interactText.text = "Press E to send Agents to Libya";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Turkey")
             {
                 interactText.text = "Press E to send Agents to Turkey";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Egypt")
             {
                 interactText.text = "Press E to send Agents to Egypt";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Romania")
             {
                 interactText.text = "Press E to send Agents to Romania";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Saudi Arabia")
             {
                 interactText.text = "Press E to send Agents to Saudi Arabia";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Ethiopia")
             {
                 interactText.text = "Press E to send Agents to Ethiopia";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Sudan")
             {
                 interactText.text = "Press E to send Agents to Sudan";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Nigeria")
             {
                 interactText.text = "Press E to send Agents to Nigeria";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Democratic Republic of the Congo")
             {
                 interactText.text = "Press E to send Agents to the Democratic Republic of the Congo";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Yemen")
             {
                 interactText.text = "Press E to send Agents to Yemen";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Afghanistan")
             {
                 interactText.text = "Press E to send Agents to Afghanistan";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "India")
             {
                 interactText.text = "Press E to send Agents to India";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Burma")
             {
                 interactText.text = "Press E to send Agents to Burma";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Thailand")
             {
                 interactText.text = "Press E to send Agents to Thailand";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "French Indochina")
             {
                 interactText.text = "Press E to send Agents to the French Indochina";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Philippines")
             {
                 interactText.text = "Press E to send Agents to the Philippines";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Australia")
             {
                 interactText.text = "Press E to send Agents to Australia";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "New Guinea")
             {
                 interactText.text = "Press E to send Agents to New Guinea";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Japan")
             {
                 interactText.text = "Press E to send Agents to Japan";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Korea")
             {
                 interactText.text = "Press E to send Agents to Korea";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "Manchuria")
             {
                 interactText.text = "Press E to send Agents to Manchuria";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else if (hit.transform.name == "China")
             {
                 interactText.text = "Press E to send Agents to China";
 
-                //if
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    levelLoader.GetComponent<LevelLoader>().LoadNextLevel();
+                }
             }
 
             else
@@ -295,6 +427,25 @@ public class RaycastingCrosshair : MonoBehaviour
             interactTextObject.SetActive(false);
 
             informationBox.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            reportReveal = !reportReveal; 
+
+            if (reportReveal)
+            {
+                Debug.Log("reportReveal is on!");
+
+                reportBox.SetActive(true); 
+            }
+
+            else
+            {
+                Debug.Log("reportReveal is off!");
+
+                reportBox.SetActive(false);
+            }
         }
     }
 }
